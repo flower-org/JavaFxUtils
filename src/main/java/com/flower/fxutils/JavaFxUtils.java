a8e79a6501361df5036cf494ff717f14a154d574
codekrolik2 <john.amirov.83@gmail.com>
2026-03-29 19:04:16 -0800

    autoResizeTableColumns added

--- src/main/java/com/flower/fxutils/JavaFxUtils.java
+++ src/main/java/com/flower/fxutils/JavaFxUtils.java
@@ -15,6 +15,7 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyCodeCombination;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.text.Text;
 import org.apache.commons.lang3.tuple.Pair;
 
 import javax.annotation.Nullable;
@@ -45,6 +46,29 @@ public class JavaFxUtils {
     }
     return null;
   };
+
+  public static void autoResizeTableColumns(TableView<?> table) {
+    //Set the right policy
+    table.setColumnResizePolicy(TableView.UNCONSTRAINED_RESIZE_POLICY);
+    table.getColumns().stream().forEach((column) -> {
+      //Minimal width = columnheader
+      Text t = new Text(column.getText());
+      double max = t.getLayoutBounds().getWidth();
+      for (int i = 0; i < table.getItems().size(); i++) {
+        //cell must not be empty
+        if (column.getCellData(i) != null) {
+          t = new Text(column.getCellData(i).toString());
+          double calcwidth = t.getLayoutBounds().getWidth();
+          //remember new max-width
+          if (calcwidth > max) {
+            max = calcwidth;
+          }
+        }
+      }
+      //set the new max-widht with some extra space
+      column.setPrefWidth(max + 10.0d);
+    });
+  }
 
   public static TextFormatter<TextFormatter.Change> createDecimalTextFormatter() {
     return new TextFormatter<>(DECIMAL_TEXT_FILTER);
